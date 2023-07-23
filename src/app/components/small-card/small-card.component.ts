f24cd22e05b549fb57ec4db1864b04597b01b453
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-23 17:32:22 -0300

    feat: adjusted responsive layout

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,11 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css'],
+  styleUrls: [
+    './small-card.component.css',
+    'small-card.responsive.component.css',
+  ],
 })
 export class SmallCardComponent {
   @Input() newsId: string = '';
   @Input() photoCover: string = '';
   @Input() photoAlt: string = '';
   @Input() smallCardTitle: string = '';
+  @Input() smallCardDescription = '';
 }
